b2b5aedf50869e0cab25748736b226b247b6c85f
Gyula Horvath <gylhrvth@gmail.com>
2024-08-29 15:28:42 +0200

    Jonathan und RaphaelK

---
+++ RaphaelK/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1724937952,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/magic-sheet0.png",
+		"images/magic-sheet1.png",
+		"media/8bit-blip1.m4a",
+		"media/8bit-blip1.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
