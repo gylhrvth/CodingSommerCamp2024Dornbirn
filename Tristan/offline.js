6cda0ff6c65a1885c3b37e351b334e8e1e8da0ad
Gyula Horvath <gylhrvth@gmail.com>
2024-08-29 16:09:52 +0200

    Tristan

---
+++ Tristan/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1724940200,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/spieler-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/bullet-sheet1.png",
+		"images/bullet-sheet2.png",
+		"images/tiledbackground12.png",
+		"images/planet1-sheet0.png",
+		"images/planet2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
