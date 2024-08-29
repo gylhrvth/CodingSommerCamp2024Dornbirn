09a641b094ceb7d3d61b3bc33454af744bad1938
Gyula Horvath <gylhrvth@gmail.com>
2024-08-29 16:01:02 +0200

    Inaaya und Irem

---
+++ Inaaya/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1724939778,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/wand-sheet0.png",
+		"images/magic-sheet0.png",
+		"images/magic-sheet1.png",
+		"images/magic-sheet2.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite3-sheet1.png",
+		"media/8bit-damage1.m4a",
+		"media/8bit-damage1.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
