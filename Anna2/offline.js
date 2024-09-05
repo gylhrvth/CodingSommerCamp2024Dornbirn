dec5f0cb280aafb71a882d5c0f53fe9fbdb962f8
Gyula Horvath <gylhrvth@gmail.com>
2024-09-05 16:03:05 +0200

    Anna

---
+++ Anna2/offline.js
@@ -0,0 +1,11 @@
+﻿{
+	"version": 1725544865,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/decke.png",
+		"images/tiledbackground.png"
+	]
+}
